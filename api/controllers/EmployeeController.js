c1764090696e510defbb9055dcde831b716a8f3a
Gaurish Gupta <67861590+guptagaurish@users.noreply.github.com>
2022-02-18 10:42:04 +0530

    Update EmployeeController.js

--- api/controllers/EmployeeController.js
+++ api/controllers/EmployeeController.js
@@ -9,7 +9,7 @@ module.exports = {
 
     create: function (req, res) {
         let data;
-          // to convert category name's first letter capital
+    
         data = { name: req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1) }
         myemployeedb.create(data).fetch().exec(function (err, employee) {
           if (err) return (err);
@@ -27,7 +27,7 @@ module.exports = {
         let query;
         let data;
         query = { "id": req.param('id') }
-        // to convert category to first letter capital, rest of them are small letters
+        
         data = { name: req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1) }
         myemployeedb.update(query,data).fetch().exec(function (err, employee) {
           employeeName = data.name;
